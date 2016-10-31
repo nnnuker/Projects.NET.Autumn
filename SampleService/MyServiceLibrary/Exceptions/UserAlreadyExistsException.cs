82a65c367419aa9ca51c29e99655d3f6c5cb29ff
Valery_Larkovich <Valery_Larkovich@EPBYMINW1491>
2016-10-31 18:38:30 +0300

    Initial

---
+++ SampleService/MyServiceLibrary/Exceptions/UserAlreadyExistsException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyServiceLibrary.Exceptions
+{
+    [Serializable]
+    public class UserAlreadyExistsException : Exception
+    {
+        public UserAlreadyExistsException()
+        {
+        }
+
+        public UserAlreadyExistsException(string message) : base(message)
+        {
+        }
+
+        public UserAlreadyExistsException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+        protected UserAlreadyExistsException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ SampleService/MyServiceLibrary/Exceptions/UserValidationException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MyServiceLibrary.Exceptions
+{
+    [Serializable]
+    public class UserValidationException : Exception
+    {
+        public UserValidationException()
+        {
+        }
+
+        public UserValidationException(string message) : base(message)
+        {
+        }
+
+        public UserValidationException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+        protected UserValidationException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
