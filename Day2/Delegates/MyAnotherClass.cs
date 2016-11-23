e413c61f14a51fab02a65f7292e53eb3257852c3
Valery_Larkovich <Valery_Larkovich@EPBYMINW1491>
2016-11-23 11:37:44 +0300

    Day2

---
+++ Day2/Delegates/MyAnotherClass.cs
@@ -0,0 +1,9 @@
+﻿namespace Delegates
+{
+    public class MyAnotherClass
+    {
+        public event SimpleDelegate Delegate1 = delegate { return 0; };
+
+        public event AdvancedDelegate Delegate2;
+    }
+}
---
+++ Day2/ValueAndReferenceTypes/Counter.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ValueAndReferenceTypes
+{
+    public struct Counter
+    {
+        private int value;
+
+        public int Value
+        {
+            get
+            {
+                return this.value;
+            }
+        }
+
+        public static IList<Counter> CreateListOfMutableStructs(int count)
+        {
+            return Enumerable.Range(1, count).Select(i => new Counter()).ToArray();
+        }
+
+        public static void IncrementCounters(Counter[] list)
+        {
+            for (int i = 0; i < list.Length; i++)
+            {
+                list[i].Increment();
+            }
+        }
+
+        public static void DecrementCounters(Counter[] list)
+        {
+            for (int i = 0; i < list.Length; i++)
+            {
+                list[i].Decrement();
+            }
+        }
+
+        public void Increment()
+        {
+            this.value++;
+        }
+
+        public void Decrement()
+        {
+            this.value--;
+        }
+    }
+}
