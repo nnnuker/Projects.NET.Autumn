7b2f36e64198c3eaf01495cb40d29c33f11078d4
Valery Larkovich <nnoookk@gmail.com>
2016-12-05 00:36:18 +0300

    network test

---
+++ SampleService/NetworkClient/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using MyServiceLibrary.Entities;
+using MyServiceLibrary.Replication;
+using MyServiceLibrary.Replication.DataSpreader;
+using MyServiceLibrary.Services;
+
+namespace NetworkClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new DataSpreaderService(new MasterService<User>(new BasicUserService()));
+
+            var sender = new NetworkDataSender("1", new IPEndPoint(IPAddress.Loopback, 8081));
+
+            client.AddDataSpreader(sender);
+
+            Console.WriteLine("Enter to send");
+            Console.ReadLine();
+
+            try
+            {
+                client.Add(new User()
+                {
+                    FirstName = "Petr",
+                    LastName = "The greatest",
+                    PersonalId = "PiterSaint",
+                    DateOfBirth = DateTime.MinValue,
+                    Visas =
+                        new VisaRecord[]
+                            {new VisaRecord() {Country = "Netherlands", Start = DateTime.MinValue, End = DateTime.MaxValue}},
+                    Gender = GenderEnum.Male
+                });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine("enter to exit");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SampleService/NetworkServer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using MyServiceLibrary.Entities;
+using MyServiceLibrary.Replication;
+using MyServiceLibrary.Replication.DataSpreader;
+using MyServiceLibrary.Services;
+
+namespace NetworkServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var slave = new DataSpreaderService(new SlaveService<User>(new BasicUserService()));
+
+            var receiver = new NetworkDataReceiver("1", new IPEndPoint(IPAddress.Loopback, 8081));
+
+            slave.AddDataSpreader(receiver);
+
+            while (true)
+            {
+                try
+                {
+                    var obj = slave.GetAll().FirstOrDefault();
+                    if (obj != null)
+                    {
+                        Console.WriteLine("Received: " + obj.FirstName);
+                        break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
